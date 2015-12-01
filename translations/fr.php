3d37d369901d137fa85383d021022cfb33364759
Grégoire Bélorgey <gregoire@antilop.fr>
2015-12-01 17:31:36 +0100

    Translations

---
+++ translations/fr.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{elasticsearch}prestashop>column_146ffe2fd9fa5bec3b63b52543793ec7'] = 'Voir plus';
+$_MODULE['<{elasticsearch}prestashop>column_c74ea6dbff701bfa23819583c52ebd97'] = 'Voir moins';
